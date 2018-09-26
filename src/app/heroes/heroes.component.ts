6eaba9eaa1c3b5208d2f845d86e06ed2f943ad1f
Михаил Трубников <metrubnikov@gmail.com>
2018-09-26 14:42:44 +1000

    Displaying a list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero'; 
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -14,9 +15,16 @@ export class HeroesComponent implements OnInit {
 	}
 	
 	// hero = 'Windstorm'
-	hero: Hero = {
-		id: 1,
-		name: 'Windstorm'
-	};
+	// hero: Hero = {
+	// 	id: 1,
+	// 	name: 'Windstorm'
+	// };
+
+	heroes = HEROES;
+
+	selectedHero: Hero;
+	onSelect(hero: Hero): void {
+		this.selectedHero = hero;
+	}
 
 }
