9112af07607a1f07b88fd2b6ede7948fa37d190b
Михаил Трубников <metrubnikov@gmail.com>
2018-09-27 14:19:57 +1000

    Routing

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,9 +11,13 @@ export class HeroService {
 
 	getHeroes(): Observable<Hero[]> {
 		//send message
-		this.messageService.add('HeroService: fetched heroes');
-
+		this.messageService.add('HeroService: полуичть список hero');
 		return of(HEROES);
+	}
+
+	getHero(id: number): Observable<Hero> {
+		this.messageService.add(`HeroService: выбрать hero id=${id}`);
+		return of(HEROES.find(hero => hero.id === id));
 	}
 
   constructor(private messageService: MessageService) { }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -24,10 +24,10 @@ export class HeroesComponent implements OnInit {
 
 	heroes: Hero[];
 
-	selectedHero: Hero;
-	onSelect(hero: Hero): void {
-		this.selectedHero = hero;
-	}
+	// selectedHero: Hero;
+	// onSelect(hero: Hero): void {
+	// 	this.selectedHero = hero;
+	// }
 
 	getHeroes(): void {
 		// this.heroes = this.heroService.getHeroes();
