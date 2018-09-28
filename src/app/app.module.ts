e6a72fd25642e1c2be94f9f279f8fd47ce097181
Михаил Трубников <metrubnikov@gmail.com>
2018-09-28 15:20:21 +1000

    Http - observable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+//эмуляция сервера
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
@@ -20,7 +26,12 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
 		BrowserModule,
 		FormsModule,
-		AppRoutingModule
+		AppRoutingModule,
+		HttpClientModule,
+
+		HttpClientInMemoryWebApiModule.forRoot(
+			InMemoryDataService, { dataEncapsulation: false }
+		)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,16 +3,27 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
+	private heroesUrl = 'api/heroes'; //URL to web api
+
 	getHeroes(): Observable<Hero[]> {
-		//send message
-		this.messageService.add('HeroService: полуичть список hero');
-		return of(HEROES);
+		//Получение из mock
+		// this.messageService.add('HeroService: полуичть список hero');
+		// return of(HEROES);
+
+		//Получение из http
+		return this.http.get<Hero[]>(this.heroesUrl)
+			.pipe(
+				//Сообщает об ошибке и возвращает безобидный результат
+				catchError(this.handleError('getHeroes', []))
+			);
 	}
 
 	getHero(id: number): Observable<Hero> {
@@ -20,5 +31,20 @@ export class HeroService {
 		return of(HEROES.find(hero => hero.id === id));
 	}
 
-  constructor(private messageService: MessageService) { }
+	private handleError<T> (operation = 'operation', result?: T) {
+		return (error: any): Observable<T> => {
+			console.error(error);
+			this.log(`${operation} ошибка: ${error.message}`);
+			return of(result as T);
+		};
+	}
+
+  constructor(
+		private messageService: MessageService,
+		private http: HttpClient) { }
+
+	private log(message: string) {
+		this.messageService.add(`HeroService: ${message}`);
+	}
+
 }
